c3eb5d875307d7c7dc1729c6e51ddca99d470c1b
Dmitriy Angve <kirirch129@gmail.com>
2023-01-19 09:20:36 +0300

    #

---
+++ app.js
@@ -0,0 +1,7 @@
+const btn = document.querySelector(".btn");
+const output = document.querySelector(".output");
+const inputVal = document.querySelector(".val");
+
+inputVal.value = "test";
+btn.textContent = "Click Me";
+btn.addEventListener("click", (e) => {});
